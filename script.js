55b52673157506b74021781d7f89d1c5a595089a
Alex <kuczera.alexander@gmail.com>
2022-05-31 15:02:31 -0600

    Wrote step for javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1.0 Your calculator should contain functions for all of the basic math operators
+//add, subtract, multiply, divide.
+
+//2.0 Create a new function 'operate' that takes
+//an operator and 2 numbers and then calls on of the
+//above functions
