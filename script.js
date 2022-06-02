dba1547190791573d8d51a4ec160a26b7b57690c
akuczera <97765892+akuczera@users.noreply.github.com>
2022-06-02 02:47:51 +0000

    Changed currentValu to parseInt

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let screenText = document.querySelector('div.display');
-console.log(screenText.textContent);
-
 let currentVal;
 let storedVal;
 let ops;
+let flag = false;
 let result;
 
 //Listens for number to be clicked
@@ -13,10 +12,21 @@ let result;
   if(ops === '=') {
     storedVal = 0;
   }
-  screenText.textContent = digit; 
-  currentVal = parseInt(digit);
-  parseFloat(currentVal).toFixed(8);
-  console.log("current value=" + currentVal);
+
+   //Below does not really work, because making op = '' doesn't work when I then click + and another number. The + does not get stored. Need different flag.
+  if(!flag){
+  currentVal = digit;
+  screenText.textContent = currentVal;
+  flag = !flag;
+    console.log(flag);
+  } else if(flag) {
+    currentVal = currentVal + digit;
+    console.log(flag);
+    screenText.textContent = currentVal;
+  }
+  // screenText.textContent = digit; 
+  // currentVal = parseInt(digit);
+  // console.log("current value=" + currentVal);
 }
 
 //Listens for operator to be clicked and stores first value
@@ -24,11 +34,11 @@ document.querySelectorAll('button.ops').forEach(button => button.addEventListene
 
  function storeOps(type) {
   if(!storedVal){
-   storedVal = currentVal;
+   storedVal = parseInt(currentVal);
   }
-  console.log("storedVal=" + storedVal);
   screenText.textContent = type;
   ops = type;
+  flag = !flag;
 }
 
 //Listens for equals to be clicked and stores second value, runs operation, displays result
@@ -40,6 +50,7 @@ document.querySelector('button.clear').addEventListener('click', clear);
 function clear() {
 currentVal = 0;
 storedVal = 0;
+ops = '';
 screenText.textContent = '';
 }
 
@@ -47,38 +58,39 @@ screenText.textContent = '';
 function add(a, b) {
   storedVal = parseFloat((a + b).toFixed(8));
   screenText.textContent = storedVal;
-  console.log("storedVal=" + storedVal);
 }
 
 function subtract(a ,b) {
   storedVal = parseFloat((a - b).toFixed(8));
   screenText.textContent = storedVal;
-  console.log("storedVal=" + storedVal);
 }
 
 function multiply(a, b) {
   storedVal = parseFloat((a * b).toFixed(8));
   screenText.textContent = storedVal;
-  console.log("storedVal=" + storedVal);
 }
 
 function divide(a, b) {
   storedVal = parseFloat((a / b).toFixed(8));
   screenText.textContent = storedVal;
-  console.log("storedVal=" + storedVal);
 }
 
 //OPERATOR Function
 function operator() {
   if(ops === '+') {
-    result = add(storedVal, currentVal);
+    result = add(storedVal, parseInt(currentVal));
   } else if(ops === '-') {
-    result = subtract(storedVal, currentVal);
+    result = subtract(storedVal, parseInt(currentVal));
   } else if(ops === 'x') {
-    result = multiply(storedVal, currentVal);
+    result = multiply(storedVal, parseInt(currentVal));
   } else if(ops === '/') {
-    result = divide(storedVal, currentVal);
+    result = divide(storedVal, parseInt(currentVal));
   }
   
   ops = '=';
 }
+
+//NEXT STEPS
+//Click num x num + num - num =, along the way it operates and equals "stops" it in a way before moving onto the next number.
+//Allow for a "string of numbers", like the number = 1832
+//Clear button makes currentValue a 0, so any more numbers clicked and it addes to the zero lol
